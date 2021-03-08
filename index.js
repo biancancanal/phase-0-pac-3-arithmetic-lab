8bffd4beb894eabc1d29ffb6444c3f295c70b292
Bianca <biancancanal@gmail.com>
2021-03-08 10:46:27 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function add() {
+    return number + 1; 
+}
+function subtract() {
+    return number - 1; 
+}
+function multiply() {
+    return number * 1; 
+}
+function divide() {
+    return number / 1; 
+}
+
+function add(a, b) {
+    return a + b; 
+}
+function subtract(a, b) {
+    return a - b; 
+}
+function multiply(a, b) {
+    return a * b; 
+}
+function divide(a, b) {
+    return a / b; 
+}
+
+function increment(n) {
+    return ++n;
+}
+function decrement(n) {
+    return --n;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
